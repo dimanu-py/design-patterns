313425517817d83100742426b3e0b0729963bfc9
dimanu <diego.martinez@frenetic.ai>
2024-02-22 20:01:51 +0100

    test: client
    This script will simulate what the client will do

---
+++ java/command_pattern/src/Client.java
@@ -0,0 +1,58 @@
+import command.CeilingFanHighCommand;
+import command.CeilingFanLowCommand;
+import command.CeilingFanMediumCommand;
+import command.GarageDoorCloseCommand;
+import command.GarageDoorOpenCommand;
+import command.LightOffCommand;
+import command.LightOnCommand;
+import command.StereoOffWithDVDCommand;
+import command.StereoOnWithCDCommand;
+import invoker.RemoteControl;
+import receiver.CeilingFan;
+import receiver.GarageDoor;
+import receiver.Light;
+import receiver.Stereo;
+
+
+public class Client {
+    
+    public static void main(String[] args){
+        RemoteControl remoteControl = new RemoteControl();
+        
+        Light livingRoomLight = new Light("Living Room");
+        Light kitchenLight = new Light("Kitchen");
+        CeilingFan ceilingFan = new CeilingFan("Living Room");
+        GarageDoor garageDoor = new GarageDoor("");
+        Stereo stereo = new Stereo("Living Room");
+        
+        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
+        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
+        
+        CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
+        CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
+        CeilingFanLowCommand ceilingFanLow = new CeilingFanLowCommand(ceilingFan);
+        
+        GarageDoorOpenCommand garageDoorUp = new GarageDoorOpenCommand(garageDoor);
+        GarageDoorCloseCommand garageDoorDown = new GarageDoorCloseCommand(garageDoor);
+        
+        StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+        StereoOffWithDVDCommand stereoOffWithDVD = new StereoOffWithDVDCommand(stereo);
+        
+        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+        remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+        remoteControl.setCommand(2, garageDoorUp, garageDoorDown);
+        remoteControl.setCommand(3, stereoOnWithCD, stereoOffWithDVD);
+        
+        System.out.println(remoteControl);
+        
+        remoteControl.onButtonWasPressed(0);
+        remoteControl.offButtonWasPressed(0);
+        remoteControl.onButtonWasPressed(1);
+        remoteControl.offButtonWasPressed(1);
+        remoteControl.onButtonWasPressed(2);
+        remoteControl.offButtonWasPressed(2);
+        remoteControl.onButtonWasPressed(3);
+    }
+}
---
+++ python/command_pattern/remote-controller/client.py
@@ -0,0 +1,62 @@
+from command import (CeilingFanHighCommand, CeilingFanLowCommand,
+                     CeilingFanMediumCommand, GarageDoorCloseCommand,
+                     GarageDoorOpenCommand, LightOffCommand, LightOnCommand,
+                     StereoOnWithCDCommand, StereoOnWithDVDCommand)
+from invoker import RemoteControl
+from receiver import CeilingFan, GarageDoor, Light, Stereo
+
+
+def main() -> None:
+    # Invoker
+    remote = RemoteControl()
+
+    # Receivers
+    living_room_light = Light("Living Room")
+    garage_door = GarageDoor()
+    stereo = Stereo("Bedroom")
+    outdoor_light = Light("Outdoor")
+    ceiling_fan = CeilingFan("Ceiling Fan")
+
+    # Commands
+    living_room_light_on = LightOnCommand(living_room_light)
+    living_room_light_off = LightOffCommand(living_room_light)
+
+    garage_door_open = GarageDoorOpenCommand(garage_door)
+    garage_door_close = GarageDoorCloseCommand(garage_door)
+
+    stereo_on_with_cd = StereoOnWithCDCommand(stereo)
+    stereo_on_with_dvd = StereoOnWithDVDCommand(stereo)
+
+    outdoor_light_on = LightOnCommand(outdoor_light)
+    outdoor_light_off = LightOffCommand(outdoor_light)
+    
+    ceiling_fan_high = CeilingFanHighCommand(ceiling_fan)
+    ceiling_fan_medium = CeilingFanMediumCommand(ceiling_fan)
+    ceiling_fan_low = CeilingFanLowCommand(ceiling_fan)
+
+    # Add command to invoker
+    remote.set_command(0, living_room_light_on, living_room_light_off)
+    remote.set_command(1, garage_door_open, garage_door_close)
+    remote.set_command(2, stereo_on_with_cd, stereo_on_with_dvd)
+    remote.set_command(3, outdoor_light_on, outdoor_light_off)
+    remote.set_command(4, ceiling_fan_high, ceiling_fan_low)
+
+    print(remote)
+
+    # Execute command
+    remote.on_button_was_pressed(0)
+    remote.off_button_was_pressed(0)
+
+    remote.on_button_was_pressed(1)
+    remote.off_button_was_pressed(1)
+
+    remote.on_button_was_pressed(2)
+    remote.off_button_was_pressed(2)
+
+    remote.on_button_was_pressed(3)
+    remote.off_button_was_pressed(3)
+    
+    
+if __name__ == "__main__":
+    main()
+    
