27aa904ad37b5b9bb267279b48efcbed96bcd204
dimanu-py <diegomtz126@gmail.com>
2024-02-28 09:44:01 +0100

    explanation: concrete states
    The concrete states will implement the actions to do in every state and how to move from one state to another.

---
+++ state_pattern/java/state/HasQuarterState.java
@@ -0,0 +1,31 @@
+package state_pattern.java.state;
+
+import state_pattern.java.GumballMachine;
+
+
+public class HasQuarterState implements State {
+    
+    private GumballMachine gumballMachine;
+
+    public HasQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public void insertQuarter() {
+        System.out.println("You can't insert another quarter");
+    }
+
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
+
+    public void turnCrank() {
+        System.out.println("You turned...");
+        gumballMachine.setState(gumballMachine.getSoldState());
+    }
+
+    public void dispense() {
+        System.out.println("No gumball dispensed");
+    }
+}
--- state_pattern/python/state.py
+++ state_pattern/python/state.py
@@ -1,8 +1,13 @@
 from abc import ABC, abstractmethod
 
+from gumball_machine import GumballMachine
+
 
 class State(ABC):
     
+    def __init__(self, gumball_machine: GumballMachine) -> None:
+        self.gumball_machine = gumball_machine
+            
     @abstractmethod
     def insert_quarter(self) -> None:
         pass
@@ -19,3 +24,69 @@ class State(ABC):
     def dispense(self) -> None:
         pass
     
+
+class SoldOutState(State):
+    
+    def insert_quarter(self):
+        print("You can't insert a quarter, the machine is sold out")
+        
+    def eject_quarter(self) -> None:
+        print("You can't eject, you haven't inserted a quarter yet")
+        
+    def turn_crank(self) -> None:
+        print("You turned, but there are no gumballs")
+        
+    def dispense(self) -> None:
+        print("No gumball dispensed")
+        
+
+class SoldState(State):
+    
+    def insert_quarter(self) -> None:
+        print("Please wait, we're already giving you a gumball")
+        
+    def eject_quarter(self) -> None:
+        print("Sorry, you already turned the crank")
+        
+    def turn_crank(self) -> None:
+        print("Turning twice doesn't get you another gumball!")
+        
+    def dispense(self) -> None:
+        self.gumball_machine.release_ball()
+        
+        if self.gumball_machine.number_gumballs == 0:
+            print("Oops, out of gumballs!")
+            self.gumball_machine.set_state(self.gumball_machine.sold_out_state)
+
+
+class NoQuarterState(State):
+    
+    def insert_quarter(self) -> None:
+        print("You inserted a quarter")
+        self.gumball_machine.set_state(self.gumball_machine.has_quarter_state)
+        
+    def eject_quarter(self) -> None:
+        print("You haven't inserted a quarter")
+        
+    def turn_crank(self) -> None:
+        print("You turned, but there's no quarter")
+    
+    def dispense(self) -> None:
+        print("You need to pay first")
+        
+        
+class HasQuarterState(State):
+    
+    def insert_quarter(self) -> None:
+        print("You can't insert another quarter")
+        
+    def eject_quarter(self) -> None:
+        print("Quarter returned")
+        self.gumball_machine.set_state(self.gumball_machine.no_quarter_state)
+        
+    def turn_crank(self) -> None:
+        print("You turned...")
+        self.gumball_machine.set_state(self.gumball_machine.sold_state)
+        
+    def dispense(self) -> None:
+        print("No gumball dispensed")
