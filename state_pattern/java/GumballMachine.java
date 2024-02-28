53560233dea9e6026243c6328d6027e9475df13c
dimanu-py <diegomtz126@gmail.com>
2024-02-28 14:34:37 +0100

    explanation: context
    The context object is the one that varies its behavior depending on its internal state. By delegating the state representation to different classes we make our client code cleaner and more extensible through polymorphism.

--- state_pattern/java/GumballMachine.java
+++ state_pattern/java/GumballMachine.java
@@ -1,80 +1,80 @@
 package state_pattern.java;
 
+import state_pattern.java.state.HasQuarterState;
+import state_pattern.java.state.NoQuarterState;
+import state_pattern.java.state.SoldOutState;
+import state_pattern.java.state.SoldState;
+import state_pattern.java.state.State;
 
 public class GumballMachine {
     
-    static final int SOLD_OUT = 0;
-    static final int NO_QUARTER = 1;
-    static final int HAS_QUARTER = 2;
-    static final int SOLD = 3;
+    State soldOutState;
+    State noQuarterState;
+    State hasQuarterState;
+    State soldState;
 
-    int state = SOLD_OUT;
+    State state = soldOutState;
     int numberGumballs = 0;
 
     public GumballMachine(int numberGumballs) {
+        
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        
         this.numberGumballs = numberGumballs;
 
         if (numberGumballs > 0) {
-            state = NO_QUARTER;
+            state = noQuarterState;
         }
     }
 
     public void insertQuarter() {
-        if (state == HAS_QUARTER) {
-            System.out.println("You can't insert another quarter");
-        } else if (state == NO_QUARTER) {
-            state = HAS_QUARTER;
-            System.out.println("You inserted a quarter");
-        } else if (state == SOLD_OUT) {
-            System.out.println("You can't insert a quarter, the machine is sold out");
-        } else if (state == SOLD) {
-            System.out.println("Please wait, we're already giving you a gumball");
-        }
+        state.insertQuarter();
     }
 
     public void ejectQuarter() {
-        if (state == HAS_QUARTER) {
-            System.out.println("Quarter returned");
-            state = NO_QUARTER;
-        } else if (state == NO_QUARTER) {
-            System.out.println("You haven't inserted a quarter");
-        } else if (state == SOLD) {
-            System.out.println("Sorry, you already turned the crank");
-        } else if (state == SOLD_OUT) {
-            System.out.println("You can't eject, you haven't inserted a quarter yet");
-        }
+        state.ejectQuarter();
     }
 
     public void turnCrank() {
-        if (state == SOLD) {
-            System.out.println("Turning twice doesn't get you another gumball!");
-        } else if (state == NO_QUARTER) {
-            System.out.println("You turned but there's no quarter");
-        } else if (state == SOLD_OUT) {
-            System.out.println("You turned, but there are no gumballs");
-        } else if (state == HAS_QUARTER) {
-            System.out.println("You turned...");
-            state = SOLD;
-            dispense();
-        }
+        state.turnCrank();
     }
 
     public void dispense() {
-        if (state == SOLD) {
-            System.out.println("A gumball comes rolling out the slot");
+        state.dispense();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (numberGumballs != 0) {
             numberGumballs = numberGumballs - 1;
-            if (numberGumballs == 0) {
-                System.out.println("Oops, out of gumballs!");
-                state = SOLD_OUT;
-            } else {
-                state = NO_QUARTER;
-            }
-        } else if (state == NO_QUARTER) {
-            System.out.println("You need to pay first");
-        } else if (state == SOLD_OUT) {
-            System.out.println("No gumball dispensed");
-        } else if (state == HAS_QUARTER) {
-            System.out.println("No gumball dispensed");
         }
     }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public int getNumberGumballs() {
+        return numberGumballs;
+    }
+
 }
--- state_pattern/python/gumball_machine.py
+++ state_pattern/python/gumball_machine.py
@@ -1,69 +1,43 @@
+from state import State, SoldOutState, HasQuarterState, SoldState, NoQuarterState
 
 
 class GumballMachine:
     
-    SOLD_OUT: int = 0
-    NO_QUARTER: int = 1
-    HAS_QUARTER: int = 2
-    SOLD: int = 3
+    sold_out_state: State
+    no_quarter_state: State
+    has_quarter_state: State
+    sold_state: State
     
     def __init__(self, number_gumballs: int) -> None:
+        
+        self.sold_out_state = SoldOutState(self)
+        self.no_quarter_state = NoQuarterState(self)
+        self.has_quarter_state = HasQuarterState(self)
+        self.sold_state = SoldState(self)
+        
+        self.state = self.sold_out_state
         self.number_gumballs = number_gumballs
         
         if number_gumballs > 0:
-            self.state = self.NO_QUARTER
+            self.state = self.no_quarter_state
             
     def insert_quarter(self) -> None:
-        
-        if self.state == self.HAS_QUARTER:
-            print("You can't insert another quarter")
-        elif self.state == self.NO_QUARTER:
-            self.state = self.HAS_QUARTER
-            print("You inserted a quarter")
-        elif self.state == self.SOLD_OUT:
-            print("You can't insert a quarter, the machine is sold out")
-        elif self.state == self.SOLD:
-            print("Please wait, we're already giving you a gumball")
+        self.state.insert_quarter()
             
     def eject_quarter(self) -> None:
-        
-        if self.state == self.HAS_QUARTER:
-            print("Quarter returned")
-            self.state = self.NO_QUARTER
-        elif self.state == self.NO_QUARTER:
-            print("You haven't inserted a quarter")
-        elif self.state == self.SOLD:
-            print("Sorry, you already turned the crank")
-        elif self.state == self.SOLD_OUT:
-            print("You can't eject, you haven't inserted a quarter yet")
+        self.state.eject_quarter()
             
     def turn_crank(self) -> None:
-        
-        if self.state == self.SOLD:
-            print("Turning twice doesn't get you another gumball")
-        elif self.state == self.NO_QUARTER:
-            print("You turned but there's no quarter")
-        elif self.state == self.SOLD_OUT:
-            print("You turned, but there are no gumballs")
-        elif self.state == self.HAS_QUARTER:
-            print("You turned...")
-            self.state = self.SOLD
-            self.dispense()
+        self.state.turn_crank()
             
     def dispense(self) -> None:
+        self.state.dispense()
         
-        if self.state == self.SOLD:
-            print("A gumball comes rolling out the slot")
-            self.number_gumballs -= 1
-            if self.number_gumballs == 0:
-                print("Oops, out of gumballs!")
-                self.state = self.SOLD_OUT
-            else:
-                self.state = self.NO_QUARTER
-        elif self.state == self.NO_QUARTER:
-            print("You need to pay first")
-        elif self.state == self.SOLD_OUT:
-            print("No gumball dispensed")
-        elif self.state == self.HAS_QUARTER:
-            print("No gumball dispensed")
+    def set_state(self, state: State) -> None:
+        self.state = state
+        
+    def release_ball(self) -> None:
+        print("A gumball comes rolling out the slot...")
+        if self.number_gumballs != 0: 
+            self.number_gumballs = self.number_gumballs - 1;
         
