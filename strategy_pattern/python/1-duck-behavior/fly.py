2c8ff5ff3726d4cb615e325de429d536342fd56d
dimanu-py <diegomtz126@gmail.com>
2024-04-04 12:21:15 +0200

    feat: create new fly behavior FlyRocketPowered
    Note that to add this new behavior we didn't need to change anything in the existing code.

--- strategy_pattern/python/1-duck-behavior/fly.py
+++ strategy_pattern/python/1-duck-behavior/fly.py
@@ -18,3 +18,9 @@ class FlyNoWay(FlyBehavior):
 
     def fly(self) -> None:
         print("I can't fly")
+
+
+class FlyRocketPowered(FlyBehavior):
+
+    def fly(self) -> None:
+        print("I'm flying with a rocket!")
